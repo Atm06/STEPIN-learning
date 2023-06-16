f656ef2e3a7b1bbc3f8b586d689d657edc4f3ea2
Ashutosh Mallick <78423220+Atm06@users.noreply.github.com>
2023-06-16 17:34:33 +0530

    Day-3-L1-Q4

---
+++ Day-3-L1-Q4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int findLargestNumber(int num);
+
+int main() {
+    int num;
+
+    printf("Enter a 4-digit number: ");
+    scanf("%d", &num);
+
+    int largestNumber = findLargestNumber(num);
+
+    printf("Largest number by deleting a single digit: %d\n", largestNumber);
+
+    return 0;
+}
+
+int findLargestNumber(int num) {
+    int largestNumber = 0;
+
+    for (int i = 0; i < 4; i++) {
+        int tempNum = num / (int)pow(10, i + 1) * (int)pow(10, i) + num % (int)pow(10, i);
+
+        if (tempNum > largestNumber) {
+            largestNumber = tempNum;
+        }
+    }
+
+    return largestNumber;
+}
